fbf6f8658f622eb741d3b0e87404f443d6b5f544
Abhaykaintura <120556340+Abhaykaintura@users.noreply.github.com>
2022-12-15 09:30:57 +0530

    Update BrowserStackSample.java

--- java_7/src/test/java/android/BrowserStackSample.java
+++ java_7/src/test/java/android/BrowserStackSample.java
@@ -1,32 +1,26 @@
 package android;
-
 import java.net.URL;
 import java.util.List;
 import java.util.function.Function;
 import java.net.MalformedURLException;
-
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-
 public class BrowserStackSample {
-
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
     	DesiredCapabilities caps = new DesiredCapabilities();
     	
     	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
-    	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
+    	caps.setCapability("browserstack.user", "abhaykaintura_E4zKuO");
+    	caps.setCapability("browserstack.key", "uWnXeqDFozHLR3pgt5gA");
     	
     	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
+    	caps.setCapability("app", "bs://080c895e103ab4cf94577738b2bda3813b176316");
     	
     	// Specify device and os_version for testing
     	caps.setCapability("device", "Google Pixel 3");
@@ -43,26 +37,10 @@ public class BrowserStackSample {
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
         		new URL("http://hub.browserstack.com/wd/hub"), caps);
         
-
-        // Test case for the BrowserStack sample Android app. 
-        // If you have uploaded your app, update the test case here. 
-        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(
-            		MobileBy.AccessibilityId("Search Wikipedia")));
-        searchElement.click();
-		AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-             ExpectedConditions.elementToBeClickable(
-            		 MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
-        insertTextElement.sendKeys("BrowserStack");
-        Thread.sleep(5000);
-        List<AndroidElement> allProductsName = driver.findElementsByClassName(
-        		"android.widget.TextView");
-        assert(allProductsName.size() > 0);
-        
+        // Write your test case statements here
         
         // Invoke driver.quit() after the test is done to indicate that the test is completed.
         driver.quit();
 		
 	}
-
 }
